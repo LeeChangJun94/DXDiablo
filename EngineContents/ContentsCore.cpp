91c34de6d75d1f0830bc675600343256da26cbc2
이창준 <naromine@naver.com>
2024-12-17 15:51:37 +0900

    이창준 : DX엔진 업데이트

---
+++ EngineContents/ContentsCore.cpp
@@ -0,0 +1,44 @@
+#include "PreCompile.h"
+#include "ContentsCore.h"
+#include <EngineCore/Level.h>
+#include "TitleGameMode.h"
+
+// #define은 그냥 무조건 복붙
+CreateContentsCoreDefine(UContentsCore);
+
+UContentsCore::UContentsCore()
+{
+}
+
+UContentsCore::~UContentsCore()
+{
+}
+
+
+void UContentsCore::EngineStart(UEngineInitData& _Data)
+{
+	// mainwindow는 아무나 건들면 안된다.
+	// 넌 컨텐츠잖아 엔진이 관리하는 윈도우라는게 존재하는지도 몰라야한다.
+
+	_Data.WindowPos = { 100, 100 };
+	_Data.WindowSize = { 1280, 720 };
+
+	// static으로 
+	// 이 된다.
+	// UEngineCore::CreateLevel<ATitleGameMode, APawn>("Titlelevel");
+	UEngineCore::CreateLevel<ATitleGameMode, APawn>("Titlelevel");
+	UEngineCore::OpenLevel("Titlelevel");
+
+	// 윈도우 크기 지정
+
+}
+
+void UContentsCore::EngineTick(float _DeltaTime)
+{
+
+}
+
+void UContentsCore::EngineEnd()
+{
+
+}
---
+++ EngineCore/EngineCore.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <EngineBase/EngineDefine.h>
+#include <EnginePlatform/EngineWindow.h>
+#include "IContentsCore.h"
+#include "Level.h"
+#include <memory>
+
+
+// 설명 :
+class UEngineCore
+{
+public:
+	// constrcuter destructer
+	ENGINEAPI UEngineCore();
+	ENGINEAPI virtual ~UEngineCore() = 0;
+
+	ENGINEAPI static void EngineStart(HINSTANCE _Instance, std::string_view _DllName);
+
+	template<typename GameModeType, typename MainPawnType>
+	static class std::shared_ptr<class ULevel> CreateLevel(std::string_view _Name)
+	{
+		// 1 유지하고 있겠죠.
+		// shared_ptr을 사용하므로 new UEngineLevel()
+		std::shared_ptr<ULevel> NewLevel = NewLevelCreate(_Name);
+		// std::make_shared
+		// new UEngineLevel();
+
+		NewLevel->SpawnActor<GameModeType>();
+		NewLevel->SpawnActor<MainPawnType>();
+
+		// 2가 됩니다
+		return NewLevel;
+	}
+
+	ENGINEAPI static void OpenLevel(std::string_view _Name);
+
+protected:
+
+private:
+	static UEngineWindow MainWindow;
+	static HMODULE ContentsDLL;
+	static std::shared_ptr<IContentsCore> Core;
+
+	static void WindowInit(HINSTANCE _Instance);
+	static void LoadContents(std::string_view _DllName);
+
+	static void EngineFrame();
+	static void EngineEnd();
+
+	ENGINEAPI static std::shared_ptr<ULevel> NewLevelCreate(std::string_view _Name);
+
+	static std::map<std::string, std::shared_ptr<class ULevel>> LevelMap;
+	static std::shared_ptr<class ULevel> CurLevel;
+	static std::shared_ptr<class ULevel> NextLevel;
+};
+
