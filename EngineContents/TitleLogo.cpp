fda2bb4a602491a727f6cde27ca5417728bdcada
이창준 <naromine@naver.com>
2024-12-26 17:30:55 +0900

    이창준 : 기하구조 업데이트

--- EngineContents/TitleLogo.cpp
+++ EngineContents/TitleLogo.cpp
@@ -1,16 +1,33 @@
 #include "PreCompile.h"
 #include "TitleLogo.h"
 #include <EngineCore/SpriteRenderer.h>
+#include <EnginePlatform/EngineInput.h>
+#include <EngineCore/DefaultSceneComponent.h>
 
 ATitleLogo::ATitleLogo()
 {
+	std::shared_ptr<UDefaultSceneComponent> Default = CreateDefaultSubObject<UDefaultSceneComponent>();
+	RootComponent = Default;
+
 	// 랜더러를 만든다.
 	LogoRenderer = CreateDefaultSubObject<USpriteRenderer>();
 	LogoRenderer->SetSprite("Player.png", 0);
 
+	// 부모가 존재하지 않는 root는 Relative든 Local이던 
+	// 결과는 같다. 
+	// 부모의 크기에 내가 영향을 받을수 있기 대문에 함수가 나뉜것이다.
+	// 부모가 없으면
+	LogoRenderer->SetRelativeScale3D({ 50, 50, 1.0f });
+	LogoRenderer->SetupAttachment(RootComponent);
 
-	SetActorRelativeScale3D({ 600.0f, 600.0f, 1.0f });
-	// SetActorLocation({ 200.0f, 100.0f, 1.0f });
+
+	Child = CreateDefaultSubObject<USpriteRenderer>();
+	Child->SetSprite("Player.png", 2);
+	// 부모의 스케일이 나에게 영향을 주면서 나는 100이 아닐수가 있다
+	Child->SetRelativeLocation({ 100.0f, 0.0f, 0.0f });
+	Child->SetScale3D({ 50.0f, 50.0f, 1.0f });
+	// Child->SetScale3D({ 50.0f, 50.0f, 1.0f });
+	Child->SetupAttachment(RootComponent);
 }
 
 ATitleLogo::~ATitleLogo()
@@ -26,5 +43,40 @@ void ATitleLogo::Tick(float _DeltaTime)
 {
 	AActor::Tick(_DeltaTime);
 
-	// AddActorLocation(FVector{ 0.001f, 0.0f, 0.0f});
+	if (UEngineInput::IsPress('A'))
+	{
+		AddRelativeLocation(FVector{ -100.0f * _DeltaTime, 0.0f, 0.0f });
+	}
+	if (UEngineInput::IsPress('D'))
+	{
+		AddRelativeLocation(FVector{ 100.0f * _DeltaTime, 0.0f, 0.0f });
+	}
+
+	if (UEngineInput::IsPress('W'))
+	{
+		AddRelativeLocation(FVector{ 0.0f, 100.0f * _DeltaTime, 0.0f });
+	}
+
+	if (UEngineInput::IsPress('S'))
+	{
+		AddRelativeLocation(FVector{ 0.0f, -100.0f * _DeltaTime, 0.0f });
+	}
+
+	if (UEngineInput::IsPress('Q'))
+	{
+		AddActorRotation(FVector{ 0.0f, 0.0f , 360.0f * _DeltaTime });
+	}
+
+	if (UEngineInput::IsPress('E'))
+	{
+		// 단 1순간만 처리되는 걸로 
+		Child->AddRelativeLocation(FVector{ 100.0f * _DeltaTime, 0.0f , 0.0f });
+	}
+
+	if (UEngineInput::IsPress('R'))
+	{
+		Child->SetLocation(FVector{ 100.0f, 0.0f , 0.0f });
+	}
+
+	// AddActorLocation(FVector{ 100.0f * _DeltaTime, 0.0f, 0.0f});
 }
